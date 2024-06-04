dc4fd990d9d10747c66417e86d8e9282d1099069
SvetlanaLukina <113930134+SvetlanaLukina@users.noreply.github.com>
2024-06-04 15:55:19 +0300

    Add files via upload

---
+++ script/doctor.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiperFeedBack", {
+    slidesPerView: 2,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
